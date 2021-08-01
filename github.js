724f4d699d949337931f533e46ae9f19f1392c0a
Andrews <57878892+and-rews@users.noreply.github.com>
2021-08-01 14:59:37 +0000

    Add files via upload

---
+++ github.js
@@ -0,0 +1,21 @@
+class Github {
+    constructor() {
+        this.client_id = 'd7bf0f18294457986986';
+        this.client_secret = 'bc52aa7b38d4aa1dd5a83ac8bda9710c52912e1c'
+        this.repos_count = 10;
+        this.repos_sort = 'created: asc';
+    }
+
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+
+        return {
+            profile,
+            repos
+        }
+    }
+}
